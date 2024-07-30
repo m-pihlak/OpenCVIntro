d4b86662391c21dce8adb3e9f5d721393061c631
m-pihlak <pihlakmer@gmail.com>
2024-07-30 23:58:02 +0300

    Loading and writing of images

---
+++ load_image.py
@@ -0,0 +1,23 @@
+import argparse
+import cv2
+
+# Load image from arguments into program
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="path to input image")
+ap.add_argument("-o", "--out", required=True,
+                help="path to output image")
+args = vars(ap.parse_args())
+
+image = cv2.imread("images/in/" + args["image"])
+(h, w, c) = image.shape[:3]
+
+print("width: {}px".format(w))
+print("height: {}px".format(h))
+print("channels: {}".format(c))
+
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+cv2.imwrite("images/out/" + args["out"], image)
